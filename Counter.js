56b00eabb7900815997c58c95ad07773be58e217
Josh Gearheart <other.josh.gearheart@gmail.com>
2019-02-06 13:31:14 -0800

    add decrement async functionality

--- Counter.js
+++ Counter.js
@@ -1,7 +1,7 @@
 /*eslint-disable no-unused-vars */
 import React, { Component, PropTypes } from 'react'
 
-const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync, onDecrementAsync }) =>
       <div>
         <button onClick={onIncrementAsync}>
           Increment after one second
@@ -14,6 +14,10 @@ const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
         <button onClick={onDecrement}>
           Decrement
         </button>
+        {' '}
+        <button onClick={onDecrementAsync}>
+          Decrement after one second
+        </button>
         <hr />
         <div>
           Clicked: {value} times
@@ -23,7 +27,9 @@ const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
   onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
+  onIncrementAsync: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+  onDecrementAsync: PropTypes.func.isRequired,
 }
 
 export default Counter
--- sagas.js
+++ sagas.js
@@ -1,15 +1,23 @@
 import { put, takeEvery, all, call } from 'redux-saga/effects'
 
- export const delay = (ms) => new Promise(res => setTimeout(res, ms))
+ export const delay = (ms) => new Promise(
+   res => setTimeout(res, ms),
+   err => console.log('error!')
+ )
 
 function* helloSaga() {
-  console.log('hello, sagas!');
+  console.log('hello, sagas! i am working!');
 }
 
-// worker Saga: will perform the async increment task
+// worker Sagas: will perform the async increment task
 export function* incrementAsync() {
   yield call(delay, 1000)
   yield put({type: 'INCREMENT'})
+}
+
+export function* decrementAsync() {
+  yield call(delay, 1000)
+  yield put({type: 'DECREMENT'})
 }
 
 // watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
@@ -17,10 +25,15 @@ export function* watchIncrementAsync() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
 
+export function* watchDecrementAsync() {
+  yield takeEvery('DECREMENT_ASYNC', decrementAsync)
+}
+
 // single entry point to start all sagas at once
 export default function* rootSaga() {
   yield all([
     helloSaga(),
-    watchIncrementAsync()
+    watchIncrementAsync(),
+    watchDecrementAsync()
   ])
 }
